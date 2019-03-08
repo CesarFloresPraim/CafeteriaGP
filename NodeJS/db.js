be658e726f1936213003b9d8418f691f35bc27a6
DESKTOP-MN2T16N\anaki <tartarknight@gmail.com>
2019-03-08 11:52:59 -0700

    jala

--- NodeJS/db.js
+++ NodeJS/db.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
+
 mongoose.connect('mongodb://localhost:27017/ProjectGammaDB', { useNewUrlParser: true }, (err) => {
     !err? console.log("MongoDB connection succeeded...") : console.log('Error in DB connection' + JSON.stringify(err, undefined, 2));
-=======
-mongoose.connect('mongodb://localhost:27017/cafeteria', { useNewUrlParser: true }, (err) => {
-    !err? console.log("MongoDB connection succeeded...") : console.log('Error in DB connecction' + JSON.stringify(err, undefined, 2));
->>>>>>> c97375209915a5d832411817943bb5d87d282900
+
 });
 
 module.exports = mongoose;
--- NodeJS/index.js
+++ NodeJS/index.js
@@ -10,9 +10,5 @@ app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:4200' }));
 app.listen(3000, () => console.log("Server started at port 3000"));
 
-<<<<<<< HEAD
-// Main address http://localhost:4200/ProjectGammaDB
-=======
 // Main address http://localhost:4200/cafeteria
->>>>>>> c97375209915a5d832411817943bb5d87d282900
 app.use('/login', userController);
